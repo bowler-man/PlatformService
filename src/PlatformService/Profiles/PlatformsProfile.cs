a0c851585156325479dff07a6de719a54af92f8c
Charl Bowler <charl.bowler@lexisnexis.co.za>
2021-09-21 13:32:00 +0200

    rearrange

---
+++ src/PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using PlatformService.Models;
+using PlatformService.Dtos;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformsProfile:Profile
+    {
+        public PlatformsProfile()
+        {
+            CreateMap<Platform,PlatformReadDto>();
+            CreateMap<PlatformCreateDto,Platform>();
+        }
+        
+    }
+
+}
